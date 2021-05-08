2fe1ace907800285115a330a681c83b9d0ab8ad0
Koments <73249213+Koments@users.noreply.github.com>
2021-05-08 15:18:23 +0300

    first commits

---
+++ app.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $('.carousel').carousel({ indicators: true,  numVisible: 7, dist: -70, padding: 10});
+})
+// $('.center').slick({
+//     centerMode: true,
+//     slidesToShow: 3,
+//     arrows: true,
+//     adaptiveHeight: true,
+//     responsive: [
+//         {
+//             breakpoint: 768,
+//             settings: {
+//                 arrows: true,
+//                 centerMode: true,
+//                 centerPadding: '40px',
+//                 slidesToShow: 3
+//             }
+//         },
+//         {
+//             breakpoint: 480,
+//             settings: {
+//                 arrows: true,
+//                 centerMode: true,
+//                 centerPadding: '40px',
+//                 slidesToShow: 1
+//             }
+//         }
+//     ]
+// });
